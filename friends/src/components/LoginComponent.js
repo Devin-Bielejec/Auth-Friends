0d4b17f5405465161f47059d62f015bbf1ef464b
devin-bielejec <devin.bieleje@gmail.com>
2019-09-10 17:11:35 -0400

    Basic login page created

---
+++ friends/src/components/LoginComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginComponent = () => {
+    return(
+        <form>
+            <input type="username"></input>
+            <input type="password"></input>
+            <button>Submit</button>
+        </form>        
+    )
+}
+
+export default LoginComponent;
